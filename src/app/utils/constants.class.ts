581ed71bbee84784663acbcd5fb4fae01ccec08e
Narbok <narbok29@gmail.com>
2022-10-13 14:53:19 -0500

    update token

--- src/app/utils/constants.class.ts
+++ src/app/utils/constants.class.ts
@@ -1,4 +1,4 @@
 export class Constants {
-    static token = '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'
+    static token = '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'
     static api = 'https://api.github.com/user'
 }
