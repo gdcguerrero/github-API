8d8bcba6a73af5cd0c93442fdc122e6378962288
Narbok <narbok29@gmail.com>
2022-10-12 20:20:16 -0500

    cambio de colores

--- src/app/utils/constants.class.ts
+++ src/app/utils/constants.class.ts
@@ -1,4 +1,4 @@
 export class Constants {
-    static token = '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'
+    static token = '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'
     static api = 'https://api.github.com/user'
 }
